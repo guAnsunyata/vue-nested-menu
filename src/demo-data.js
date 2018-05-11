f10a5cbc403c68cd9f69d5fe2f44456ee4de6849
guansunyata <guansunyata@gmail.com>
2018-05-11 11:43:10 +0800

    Add demo menu data

---
+++ src/demo-data.js
@@ -0,0 +1,97 @@
+
+const data = {
+    title: '首頁',
+    children: [
+        {
+            title: `Today's Deals`,
+            children: [],
+        },
+        {
+            title: `Interesting Finds`,
+            children: [],
+        },
+        {
+            title: `Your Recommendations`,
+            children: [],
+        },
+        {
+            title: `Shop By Department`,
+            children: [
+                {
+                    title: `Amazon Music`,
+                    children: [
+                        {
+                            title: `Amazon Music Unlimited`,
+                            children: [],
+                        },
+                        {
+                            title: `Prime Music`,
+                            children: [],
+                        },
+                        {
+                            title: `CDs and Vinyl`,
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    title: `Prime Vedios`,
+                    children: [
+                        {
+                            title: `All Vedio`,
+                            children: [],
+                        },
+                        {
+                            title: `Included with Prime`,
+                            children: [],
+                        },
+                        {
+                            title: `Rent or Buy`,
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    title: `Treasure Truck`,
+                    children: [],
+                },
+                {
+                    title: `Amazon Restaurants`,
+                    children: [
+                        {
+                            title: `Thai`,
+                            children: [],
+                        },
+                        {
+                            title: `Chinese`,
+                            children: [],
+                        },
+                        {
+                            title: `American`,
+                            children: [],
+                        },
+                        {
+                            title: `Indian`,
+                            children: [],
+                        },
+                        {
+                            title: `Popular Restaurants`,
+                            children: [
+                                {
+                                    title: `Popular Restaurants 1`,
+                                    children: [],
+                                },
+                                {
+                                    title: `Popular Restaurants 2`,
+                                    children: [],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
+export default data;
