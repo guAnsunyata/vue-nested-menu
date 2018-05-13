c154b252d0909bd843cb16e78b11a55d2409e0c0
guansunyata <guansunyata@gmail.com>
2018-05-13 23:26:16 +0800

    Correct rollup format and update dist

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,12 @@ import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
 import vue from 'rollup-plugin-vue';
 import nodeGlobals from 'rollup-plugin-node-globals';
-import bubleConfig from '../config/buble.config.js'
+import bubleConfig from './config/buble.config.js';
 
 export default {
     entry: 'src/index.js',
     dest: 'dist/vue-nested-menu.js',
-    format: 'iife',
+    format: 'umd',
     moduleName: 'VueNestedMenu',
     sourceMap: false,
     useStrict: false,
