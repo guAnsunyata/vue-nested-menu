e0c5b4a41a843c6bc09082e8f0f675e2b1c14e0e
guansunyata <guansunyata@gmail.com>
2018-05-13 22:48:18 +0800

    Add exmaple draft and dist

---
+++ example/single-file-component/MyMenu.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Menu :list="list" />
+    </div>
+</template>
+
+<script>
+import Menu from 'vue-nested-menu/vue-nested-menu.vue';
+
+export default {
+    components: {
+        Menu,
+    },
+    data() {
+        return {
+            list: [
+                // ...
+            ],
+        };
+    },
+};
+</script>
